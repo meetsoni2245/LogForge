9fa04b089aa85f763d2df5ec01bb9c32117fa43e
Meet Soni <meetsoni2245@gmail.com>
2026-08-28 18:58:31 +0530

    refactor: use centralized database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,10 @@
 import "dotenv/config";
+import { env } from "./env.js";
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: env.DATABASE_URL,
 });
 
 const prisma = new PrismaClient({
